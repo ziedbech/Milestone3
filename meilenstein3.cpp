6882b771d47562e1ed4a28afc3715ea161c863fb
ziedbech <44369312+ziedbech@users.noreply.github.com>
2019-01-09 21:03:58 +0100

    Update meilenstein3.cpp

--- meilenstein3.cpp
+++ meilenstein3.cpp
@@ -6,18 +6,40 @@
 #include "Neuron.h"
 #include "aktivierungsfunktion.h"
 
-int main()
-{
-
-	
-	f(2.8687); // Aktivierungsfunktion. Beispielaufruf 
-
-	system("pause");
-	//return 0;
+#include <string>
+#include <random>
 
 
+void matrixgenerator (int x, int y) {  // x Zeile y Spalte
 
+
+    float matrix[x][y];
+
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0,2.0);
+
+    int p[]={};
+
+    for (int i=0; i<x; ++i) {
+        for (int j=0; j<y; ++j) {
+            double number = distribution(generator);
+            if ((number>=0.0)&&(number<10.0)) ++p[int(number)];
+            matrix[i][j] = number ;
+
+        } }
+
+    for (int i=0; i<x; ++i) {
+        for (int j=0; j<y; ++j) {
+            std::cout << matrix[i][j] << std::endl;
+        }}
 
 
 }
 
+
+
+int main()
+{
+    matrixgenerator(5, 5);
+    return 0;
+}
